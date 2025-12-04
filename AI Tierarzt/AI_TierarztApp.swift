5eca7b7c6602c12e99604d97980ab539e27f5636
test <testapp>
2025-12-04 17:46:07 +0100

    Initial Commit

---
+++ AI Tierarzt/AI_TierarztApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_TierarztApp.swift
+//  AI Tierarzt
+//
+//  Created by Largou on 04.12.25.
+//
+
+import SwiftUI
+
+@main
+struct AI_TierarztApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
